051aaa20b98f0ac6cb747ec6b4f11bbcf82a3600
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2023-07-12 22:00:13 +0500

    Update Main.java

--- Урок 3. Классы и объекты/HW/src/Main.java
+++ Урок 3. Классы и объекты/HW/src/Main.java
@@ -1,1 +1,18 @@
+import employees.Employee;
+import employees.Manager;
 
+public class Main {
+    public static void main(String[] args) {
+        Employee[] employees = new Employee[3];
+        employees[0] = new Employee("Петр", 2000);
+        employees[1] = new Manager("Алиса", 3000);
+        employees[2] = new Employee("Борис", 2500);
+
+        double salaryIncrease = 1500;
+        Employee.increaseSalary(employees, salaryIncrease);
+
+        for (Employee employee : employees) {
+            System.out.println("Имя: " + employee.getName() + ", Зарплата: " + employee.getSalary());
+        }
+    }
+}
